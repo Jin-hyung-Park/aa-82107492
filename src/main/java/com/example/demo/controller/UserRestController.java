cec82abf9d5318aa6bcd8024c030f759e79e2049
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-28 21:47:05 +0900

    Create UserRestController.java

---
+++ src/main/java/com/example/demo/controller/UserRestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+    @GetMapping("/user")
+    public String getUser() {
+        return "Id : 82107492";
+    }
+}
